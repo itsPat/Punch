429754523b0188f6ed4fcd123e9153cdc2ded9e2
Pat <patrick.trudel@me.com>
2019-06-24 20:07:06 -0400

    View Controllers Setup

---
+++ Punch/AdminTodayViewController.swift
@@ -0,0 +1,66 @@
+//
+//  AdminViewController.swift
+//  Punch
+//
+//  Created by Patrick Trudel on 2019-06-24.
+//  Copyright © 2019 Patrick Trudel. All rights reserved.
+//
+
+import UIKit
+
+class AdminTodayViewController: UIViewController {
+    
+    let items: [Employee] = [
+        Employee(name: "Pat Trudel", shift: [Shift(start: Date(), finish: Date())], amountOwed: 1600),
+        Employee(name: "Pat Trudel", shift: [Shift(start: Date(), finish: Date())], amountOwed: 1600),
+        Employee(name: "Pat Trudel", shift: [Shift(start: Date(), finish: Date())], amountOwed: 1600),
+        Employee(name: "Pat Trudel", shift: [Shift(start: Date(), finish: Date())], amountOwed: 1600),
+        Employee(name: "Pat Trudel", shift: [Shift(start: Date(), finish: Date())], amountOwed: 1600),
+        Employee(name: "Pat Trudel", shift: [Shift(start: Date(), finish: Date())], amountOwed: 1600),
+        Employee(name: "Pat Trudel", shift: [Shift(start: Date(), finish: Date())], amountOwed: 1600),
+        Employee(name: "Pat Trudel", shift: [Shift(start: Date(), finish: Date())], amountOwed: 1600),
+        Employee(name: "Pat Trudel", shift: [Shift(start: Date(), finish: Date())], amountOwed: 1600),
+        Employee(name: "Pat Trudel", shift: [Shift(start: Date(), finish: Date())], amountOwed: 1600),
+        Employee(name: "Pat Trudel", shift: [Shift(start: Date(), finish: Date())], amountOwed: 1600),
+        Employee(name: "Pat Trudel", shift: [Shift(start: Date(), finish: Date())], amountOwed: 1600),
+        Employee(name: "Pat Trudel", shift: [Shift(start: Date(), finish: Date())], amountOwed: 1600),
+        Employee(name: "Pat Trudel", shift: [Shift(start: Date(), finish: Date())], amountOwed: 1600),
+        Employee(name: "Pat Trudel", shift: [Shift(start: Date(), finish: Date())], amountOwed: 1600),
+        Employee(name: "Pat Trudel", shift: [Shift(start: Date(), finish: Date())], amountOwed: 1600),
+        Employee(name: "Pat Trudel", shift: [Shift(start: Date(), finish: Date())], amountOwed: 1600),
+        Employee(name: "Pat Trudel", shift: [Shift(start: Date(), finish: Date())], amountOwed: 1600),
+        Employee(name: "Pat Trudel", shift: [Shift(start: Date(), finish: Date())], amountOwed: 1600),
+        Employee(name: "Pat Trudel", shift: [Shift(start: Date(), finish: Date())], amountOwed: 1600),
+        Employee(name: "Pat Trudel", shift: [Shift(start: Date(), finish: Date())], amountOwed: 1600),
+    ]
+
+}
+
+extension AdminTodayViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width * 0.8, height: view.frame.width * 0.2)
+    }
+}
+
+extension AdminTodayViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as? CollectionViewCell else { return UICollectionViewCell() }
+        cell.titleLabel.text = items[indexPath.item].name
+        cell.detailLabel.text = items[indexPath.item].shift.first!.start.description
+        cell.layer.backgroundColor = UIColor.white.cgColor
+        cell.layer.cornerRadius = cell.frame.height * 0.2
+        cell.layer.shadowColor = Int.random(in: 1...6) == 1 ? UIColor.red.cgColor : UIColor.gray.cgColor
+        cell.layer.shadowOffset = CGSize(width: 1.0, height: 3.0)
+        cell.layer.shadowRadius = 2.0
+        cell.layer.shadowOpacity = 1.0
+        cell.layer.masksToBounds = false
+        return cell
+    }
+    
+}
+
---
+++ Punch/EmployeeViewController.swift
@@ -0,0 +1,83 @@
+//
+//  EmployeeViewController.swift
+//  Punch
+//
+//  Created by Patrick Trudel on 2019-06-24.
+//  Copyright © 2019 Patrick Trudel. All rights reserved.
+//
+
+import UIKit
+
+struct Shift {
+    let start: Date
+    let finish: Date
+}
+
+struct Employee {
+    let name: String
+    let shift: [Shift]
+    let amountOwed: Int
+}
+
+class EmployeeViewController: UIViewController {
+
+    
+    let items: [Shift] = [
+        Shift(start: Date(), finish: Date()),
+        Shift(start: Date(), finish: Date()),
+        Shift(start: Date(), finish: Date()),
+        Shift(start: Date(), finish: Date()),
+        Shift(start: Date(), finish: Date()),
+        Shift(start: Date(), finish: Date()),
+        Shift(start: Date(), finish: Date()),
+        Shift(start: Date(), finish: Date()),
+        Shift(start: Date(), finish: Date()),
+        Shift(start: Date(), finish: Date()),
+        Shift(start: Date(), finish: Date()),
+        Shift(start: Date(), finish: Date()),
+    ]
+    
+    
+
+}
+
+extension EmployeeViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width * 0.8, height: view.frame.width * 0.2)
+    }
+}
+
+extension EmployeeViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as? CollectionViewCell else { return UICollectionViewCell() }
+        cell.titleLabel.text = formatToDateString(date: items[indexPath.item].start)
+        cell.detailLabel.text = "\(formatToHourMinutesString(date: items[indexPath.item].start)) - \(formatToHourMinutesString(date: items[indexPath.item].finish))"
+        cell.layer.backgroundColor = UIColor.white.cgColor
+        cell.layer.cornerRadius = cell.frame.height * 0.2
+        cell.layer.shadowColor = Int.random(in: 1...6) == 1 ? UIColor.red.cgColor : UIColor.gray.cgColor
+        cell.layer.shadowOffset = CGSize(width: 1.0, height: 3.0)
+        cell.layer.shadowRadius = 2.0
+        cell.layer.shadowOpacity = 1.0
+        cell.layer.masksToBounds = false
+        return cell
+    }
+    
+    func formatToDateString(date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE, MMMM dd"
+        return dateFormatter.string(from: date)
+    }
+    
+    func formatToHourMinutesString(date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "HH:mm"
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        return dateFormatter.string(from: date)
+    }
+
+}
