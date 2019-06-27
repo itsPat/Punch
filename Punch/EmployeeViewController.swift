1f3252e66d24a656970432ee0168eec562d542cb
Russell Weber <rwjk16@gmail.com>
2019-06-27 18:58:51 -0400

    changing style

--- Punch/EmployeeViewController.swift
+++ Punch/EmployeeViewController.swift
@@ -24,6 +24,8 @@ extension State {
     }
 }
 
+//MARK: - Structs
+
 struct Shift {
     let start: Date
     let finish: Date
@@ -36,41 +38,15 @@ struct Employee {
 }
 
 class EmployeeViewController: UIViewController {
-    
-    @IBOutlet weak var collectionView: UICollectionView!
+    //MARK: - Outlets
+    @IBOutlet weak var calendarBottomConstraintView: UIView!
     @IBOutlet weak var calendarContainerView: UIView!
     
-    var headerView: CustomCollectionReusableView!
+    // MARK: - Constants
     
-    var calendarSize = CGSize()
-    
-    
-    override func viewDidLoad() {
-        calendarSize = CGSize(width: view.frame.width * 0.8, height: view.frame.width * 0.8)
-        collectionView.register(CustomCollectionReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "calendarView")
-    }
-    
-    override func viewDidLayoutSubviews() {
-        collectionView.contentInset = UIEdgeInsets(top: 20, left: 0, bottom: 0, right: 0)
-        //        calendarContainerView.setStandardShadow()
-        //        calendarContainerView.setGradientBackground(colorOne: CustomColors.blue, colorTwo: CustomColors.darkBlue)
-    }
-    
-//    func setupCalendarView() {
-//        headerView.calendar.appearance.headerTitleFont = UIFont.boldSystemFont(ofSize: 18)
-//        headerView.calendar.appearance.titleFont = UIFont.boldSystemFont(ofSize: 12)
-//        headerView.calendar.backgroundColor = UIColor.clear
-//        //            calendar.layer.cornerRadius = calendar.frame.width * 0.05
-//        //            calendar.setGradientBackground(colorOne: CustomColors.blue, colorTwo: CustomColors.darkBlue)
-//        //            calendar.setStandardShadow()
-//    }
+    private let popupOffset: CGFloat = 440
     
     let items: [Shift] = [
-        
-        //        [
-        //            Employee(name: "Pat Trudel", shift: [Shift(start: Date(), finish: Date())], amountOwed: 1600)
-        //        ],
-        
         Shift(start: Date(), finish: Date()),
         Shift(start: Date(), finish: Date()),
         Shift(start: Date(), finish: Date()),
@@ -85,21 +61,298 @@ class EmployeeViewController: UIViewController {
         Shift(start: Date(), finish: Date()),
     ]
     
+    // MARK: - Views
+    
+    private lazy var calendarView: FSCalendar = {
+        let calendarView = FSCalendar()
+        return calendarView
+    }()
+    
+    private lazy var overlayView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        view.alpha = 0
+        return view
+    }()
+    
+    private lazy var popupView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
+        view.layer.shadowColor = CustomColors.blue.cgColor
+        view.layer.shadowOpacity = 0.1
+        view.layer.shadowRadius = 10
+        return view
+    }()
+    
+    private lazy var closedTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Upcoming Shifts"
+        label.font = UIFont.systemFont(ofSize: 16, weight: UIFont.Weight.medium)
+        label.textColor = CustomColors.blue
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private lazy var openTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Upcoming Shifts"
+        label.font = UIFont.systemFont(ofSize: 24, weight: UIFont.Weight.heavy)
+        label.textColor = .black
+        label.textAlignment = .center
+        label.alpha = 0
+        label.transform = CGAffineTransform(scaleX: 0.65, y: 0.65).concatenating(CGAffineTransform(translationX: 0, y: -15))
+        return label
+    }()
+    
+    private lazy var collectionView: UICollectionView = {
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 60, height: 30)
+        let view = UICollectionView(frame: (CGRect(x: 0, y: 0, width: self.popupView.frame.size.width - 10, height: self.popupView.frame.height)), collectionViewLayout:layout)
+        view.register(CollectionViewCell.self, forCellWithReuseIdentifier: "CollectionViewCell")
+        return view
+    }()
+    
+    //MARK: - VC Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        layout()
+        popupView.addGestureRecognizer(panRecognizer)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        collectionView.contentInset = UIEdgeInsets(top: 20, left: 0, bottom: 0, right: 0)
+    }
+    
     //TODO: When a cell is tapped, check that the date is today, if so punch the user in.
+   
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
     
+    // MARK: - Layout
     
+    private var bottomConstraint = NSLayoutConstraint()
+    
+    private func layout() {
+        
+        calendarView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(calendarView)
+        calendarView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        calendarView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        calendarView.topAnchor.constraint(equalTo: calendarContainerView.bottomAnchor).isActive = true
+        calendarView.bottomAnchor.constraint(equalTo: calendarBottomConstraintView.topAnchor).isActive = true
+        //        calendarView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 1)
+        
+        overlayView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(overlayView)
+        overlayView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        overlayView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        overlayView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        overlayView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+        popupView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(popupView)
+        popupView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        popupView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        bottomConstraint = popupView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: popupOffset)
+        bottomConstraint.isActive = true
+        popupView.heightAnchor.constraint(equalToConstant: 700).isActive = true
+        
+        closedTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        popupView.addSubview(closedTitleLabel)
+        closedTitleLabel.leadingAnchor.constraint(equalTo: popupView.leadingAnchor).isActive = true
+        closedTitleLabel.trailingAnchor.constraint(equalTo: popupView.trailingAnchor).isActive = true
+        closedTitleLabel.topAnchor.constraint(equalTo: popupView.topAnchor, constant: 20).isActive = true
+        
+        openTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        popupView.addSubview(openTitleLabel)
+        openTitleLabel.leadingAnchor.constraint(equalTo: popupView.leadingAnchor).isActive = true
+        openTitleLabel.trailingAnchor.constraint(equalTo: popupView.trailingAnchor).isActive = true
+        openTitleLabel.topAnchor.constraint(equalTo: popupView.topAnchor, constant: 30).isActive = true
+        
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        popupView.addSubview(collectionView)
+        collectionView.leadingAnchor.constraint(equalTo: popupView.leadingAnchor).isActive = true
+        collectionView.trailingAnchor.constraint(equalTo: popupView.trailingAnchor).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: popupView.bottomAnchor).isActive = true
+        collectionView.topAnchor.constraint(equalTo: openTitleLabel.bottomAnchor, constant: 20).isActive = true
+    }
+    
+    // MARK: - Animation
+    
+    /// The current state of the animation. This variable is changed only when an animation completes.
+    private var currentState: State = .closed
+    
+    /// All of the currently running animators.
+    private var runningAnimators = [UIViewPropertyAnimator]()
+    
+    /// The progress of each animator. This array is parallel to the `runningAnimators` array.
+    private var animationProgress = [CGFloat]()
+    
+    private lazy var panRecognizer: InstantPanGestureRecognizer = {
+        let recognizer = InstantPanGestureRecognizer()
+        recognizer.addTarget(self, action: #selector(popupViewPanned(recognizer:)))
+        return recognizer
+    }()
+    
+    /// Animates the transition, if the animation is not already running.
+    private func animateTransitionIfNeeded(to state: State, duration: TimeInterval) {
+        
+        // ensure that the animators array is empty (which implies new animations need to be created)
+        guard runningAnimators.isEmpty else { return }
+        
+        // an animator for the transition
+        let transitionAnimator = UIViewPropertyAnimator(duration: duration, dampingRatio: 1, animations: {
+            switch state {
+            case .open:
+                self.bottomConstraint.constant = 0
+                self.popupView.layer.cornerRadius = 20
+                self.overlayView.alpha = 0.5
+                self.closedTitleLabel.transform = CGAffineTransform(scaleX: 1.6, y: 1.6).concatenating(CGAffineTransform(translationX: 0, y: 15))
+                self.openTitleLabel.transform = .identity
+            case .closed:
+                self.bottomConstraint.constant = self.popupOffset
+                self.popupView.layer.cornerRadius = 0
+                self.overlayView.alpha = 0
+                self.closedTitleLabel.transform = .identity
+                self.openTitleLabel.transform = CGAffineTransform(scaleX: 0.65, y: 0.65).concatenating(CGAffineTransform(translationX: 0, y: -15))
+            }
+            self.view.layoutIfNeeded()
+        })
+        
+        // the transition completion block
+        transitionAnimator.addCompletion { position in
+            
+            // update the state
+            switch position {
+            case .start:
+                self.currentState = state.opposite
+            case .end:
+                self.currentState = state
+            case .current:
+                ()
+            }
+            
+            // manually reset the constraint positions
+            switch self.currentState {
+            case .open:
+                self.bottomConstraint.constant = 0
+            case .closed:
+                self.bottomConstraint.constant = self.popupOffset
+            }
+            
+            // remove all running animators
+            self.runningAnimators.removeAll()
+            
+        }
+        
+        // an animator for the title that is transitioning into view
+        let inTitleAnimator = UIViewPropertyAnimator(duration: duration, curve: .easeIn, animations: {
+            switch state {
+            case .open:
+                self.openTitleLabel.alpha = 1
+            case .closed:
+                self.closedTitleLabel.alpha = 1
+            }
+        })
+        inTitleAnimator.scrubsLinearly = false
+        
+        // an animator for the title that is transitioning out of view
+        let outTitleAnimator = UIViewPropertyAnimator(duration: duration, curve: .easeOut, animations: {
+            switch state {
+            case .open:
+                self.closedTitleLabel.alpha = 0
+            case .closed:
+                self.openTitleLabel.alpha = 0
+            }
+        })
+        outTitleAnimator.scrubsLinearly = false
+        
+        // start all animators
+        transitionAnimator.startAnimation()
+        inTitleAnimator.startAnimation()
+        outTitleAnimator.startAnimation()
+        
+        // keep track of all running animators
+        runningAnimators.append(transitionAnimator)
+        runningAnimators.append(inTitleAnimator)
+        runningAnimators.append(outTitleAnimator)
+        
+    }
+    
+    @objc private func popupViewPanned(recognizer: UIPanGestureRecognizer) {
+        switch recognizer.state {
+        case .began:
+            
+            // start the animations
+            animateTransitionIfNeeded(to: currentState.opposite, duration: 1)
+            
+            // pause all animations, since the next event may be a pan changed
+            runningAnimators.forEach { $0.pauseAnimation() }
+            
+            // keep track of each animator's progress
+            animationProgress = runningAnimators.map { $0.fractionComplete }
+            
+        case .changed:
+            
+            // variable setup
+            let translation = recognizer.translation(in: popupView)
+            var fraction = -translation.y / popupOffset
+            
+            // adjust the fraction for the current state and reversed state
+            if currentState == .open { fraction *= -1 }
+            if runningAnimators[0].isReversed { fraction *= -1 }
+            
+            // apply the new fraction
+            for (index, animator) in runningAnimators.enumerated() {
+                animator.fractionComplete = fraction + animationProgress[index]
+            }
+            
+        case .ended:
+            
+            // variable setup
+            let yVelocity = recognizer.velocity(in: popupView).y
+            let shouldClose = yVelocity > 0
+            
+            // if there is no motion, continue all animations and exit early
+            if yVelocity == 0 {
+                runningAnimators.forEach { $0.continueAnimation(withTimingParameters: nil, durationFactor: 0) }
+                break
+            }
+            
+            // reverse the animations based on their current state and pan motion
+            switch currentState {
+            case .open:
+                if !shouldClose && !runningAnimators[0].isReversed { runningAnimators.forEach { $0.isReversed = !$0.isReversed } }
+                if shouldClose && runningAnimators[0].isReversed { runningAnimators.forEach { $0.isReversed = !$0.isReversed } }
+            case .closed:
+                if shouldClose && !runningAnimators[0].isReversed { runningAnimators.forEach { $0.isReversed = !$0.isReversed } }
+                if !shouldClose && runningAnimators[0].isReversed { runningAnimators.forEach { $0.isReversed = !$0.isReversed } }
+            }
+            
+            // continue all animations
+            runningAnimators.forEach { $0.continueAnimation(withTimingParameters: nil, durationFactor: 0) }
+            
+        default:
+            ()
+        }
+    }
     
 }
 
+//MARK: - Collection View Delegate Flow Layout
 extension EmployeeViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: view.frame.width * 0.8, height: view.frame.width * 0.2)
     }
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
-        return calendarSize
-    }
 }
+
+//MARK: - UICollection View Data Source
 
 extension EmployeeViewController: UICollectionViewDataSource {
     
@@ -117,31 +370,21 @@ extension EmployeeViewController: UICollectionViewDataSource {
         var titleLabelText = ""
         var detailLabelText = ""
         
-        //        if indexPath.section == 0 {
-        //            // The $ Amount owed for this employee.
-        //            let item = items[indexPath.section][indexPath.row] as! Employee
-        //            titleLabelText = "$\(item.amountOwed)"
-        //            detailLabelText = "80 hours worked"
-        //        } else {
         // The Upcoming shifts for this employee.
         let item = items[indexPath.row]
         titleLabelText = formatToDateString(date: item.start)
         detailLabelText = "\(formatToHourMinutesString(date: item.start)) - \(formatToHourMinutesString(date: item.finish))"
         
         
-        cell.titleLabel.text = titleLabelText
-        cell.detailLabel.text = detailLabelText
+        cell.textLabel.text = titleLabelText
+        cell.subtitleLabel.text = detailLabelText
         cell.setCornerRadius()
         cell.setStandardShadow()
         return cell
     }
     
-    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        headerView = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "calendarView", for: indexPath) as! CustomCollectionReusableView
-        headerView.calendar.dataSource = self
-        headerView.calendar.delegate = self
-        return headerView
-    }
+    
+    //MARK: - Helper Methods
     
     func formatToDateString(date: Date) -> String {
         let dateFormatter = DateFormatter()
@@ -158,12 +401,26 @@ extension EmployeeViewController: UICollectionViewDataSource {
     
 }
 
+//MARK: Calendar Delegate + Data Source
+
 extension EmployeeViewController: FSCalendarDelegate, FSCalendarDataSource {
     
     func calendar(_ calendar: FSCalendar, boundingRectWillChange bounds: CGRect, animated: Bool) {
         DispatchQueue.main.async {
             self.view.layoutIfNeeded()
         }
+    }
+    
+}
+
+// MARK: - InstantPanGestureRecognizer
+/// A pan gesture that enters into the `began` state on touch down instead of waiting for a touches moved event.
+class InstantPanGestureRecognizer: UIPanGestureRecognizer {
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent) {
+        if (self.state == UIGestureRecognizer.State.began) { return }
+        super.touchesBegan(touches, with: event)
+        self.state = UIGestureRecognizer.State.began
     }
     
 }
