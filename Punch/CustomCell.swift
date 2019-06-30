87ac64037eca05d143e85325adfb7c74dd46ffb4
Russell Weber <rwjk16@gmail.com>
2019-06-30 10:41:24 -0400

    redid cells

--- Punch/CustomCell.swift
+++ Punch/CustomCell.swift
@@ -19,10 +19,14 @@ class CustomCell: UICollectionViewCell {
     }
     
     public func configure(with model: Shift1) {
-        let startTime = model.startTime
-        let finishTime = model.finishTime
-        dayLabel.text = finishTime
-        timeLabel.text = startTime + " - " + finishTime
+        guard let startTimeInterval = TimeInterval(model.startTime) else { return }
+        guard let finishTimeInterval = TimeInterval(model.startTime) else { return }
+        let startTime = Date(timeIntervalSince1970: startTimeInterval)
+        let finishTime = Date(timeIntervalSince1970: finishTimeInterval )
+        let dayTextLabel = formatToDateString(date: startTime)
+        let timeLabelText = (formatToHourMinutesString(date: startTime) + " - " + formatToHourMinutesString(date: finishTime))
+        dayLabel.text = dayTextLabel
+        timeLabel.text = timeLabelText
         print(Date())
     }
     
